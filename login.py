844bff6fa125cf8799e7d6a3a9cd54ba9ad60169
JackAlvites12 <jack_manuel_alvites_12@hotmail.com>
2023-05-03 12:41:48 -0500

    login finalizado

--- login.py
+++ login.py
@@ -1,1 +1,10 @@
-print("Soy el login")
+print("Bienvenido al sistema")
+user = input("Ingrese su usuario: ")
+password = input("Ingrese su contraseña: ")
+
+if(user == "jack" and password == "123" ):{
+    print("Bienvenido! " +  user)
+}
+else :{
+    print("Credenciales incorrectas")
+}
