669930c4b9099c0de9f5ec6c81464ddd800df236
JackAlvites12 <jack_manuel_alvites_12@hotmail.com>
2023-05-06 12:00:17 -0500

    Registrar finalizado

---
+++ registrate.py
@@ -0,0 +1,6 @@
+print("**** REGISTRATE ****")
+user = input("Ingrese su usuario: ")
+password = input("Ingrese su password: ")
+
+print ("***Bienvenido al Sistema***")
+print(f"Tu usuario es: {user}")
